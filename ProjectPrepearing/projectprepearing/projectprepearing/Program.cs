8c253a3c65a782f66031429c2c7cbacf664035f6
Popovit <87820730+Popovit@users.noreply.github.com>
2021-07-22 08:51:13 -0700

    Add files via upload

---
+++ ProjectPrepearing/projectprepearing/projectprepearing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectprepearing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = Console.ReadLine();
+            int project = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Name + " " + "needs" + " " + project + " "+ "to"+ " "+ "success"+".");
+
+            Console.ReadLine();
+        }
+    }
+}
