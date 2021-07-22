9e37dfdd99482bbd78fc7412021c68b31bad5ae7
Popovit <87820730+Popovit@users.noreply.github.com>
2021-07-22 14:07:36 -0700

    Add files via upload

---
+++ FirstStepsInCodingWithCSharp/Depozit/Depozit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depozit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depSUM = double.Parse(Console.ReadLine());
+            double depSROK = double.Parse(Console.ReadLine());
+            double procentgodishen = double.Parse(Console.ReadLine());
+            double natlihwa;
+            natlihwa = depSUM * 5.7/100; //Natrupana Lihwa
+            double ivan;
+            ivan = natlihwa / 12;
+            double obsht = depSUM + ((depSROK) * ivan);
+            
+
+
+
+            
+                Console.WriteLine($"Вие получавате {obsht} в края на депозитния период !");
+
+
+            Console.ReadLine();
+        }
+    }
+}
