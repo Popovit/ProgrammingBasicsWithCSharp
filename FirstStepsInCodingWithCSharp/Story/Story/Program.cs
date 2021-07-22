067d6c089a718ad4839fd87ca55b700bb91d6770
Popovit <87820730+Popovit@users.noreply.github.com>
2021-07-22 09:15:50 -0700

    Add files via upload

---
+++ FirstStepsInCodingWithCSharp/Story/Story/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = Console.ReadLine();
+            string LastName = Console.ReadLine();
+            int Age = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+            Console.WriteLine(Name + " " + LastName + " " + "е" + " " + "на" + " " + Age + "години" + " " + "от" + " " + town + "!");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
---
+++ FirstStepsInCodingWithCSharp/Zoo/Zoo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double ivan = double.Parse(Console.ReadLine());
+            double gosho = double.Parse(Console.ReadLine());
+            double dogs = 2.5;
+            double others = 4;
+
+            double sum = (ivan * dogs) + (gosho * others);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+    }
+}
