38e27e934576b85bbac2f54c7acba14afd5fadb0
Popovit <87820730+Popovit@users.noreply.github.com>
2021-07-22 14:19:37 -0700

    Add files via upload

---
+++ FirstStepsInCodingWithCSharp/ZaduljitelnaLiteratura/ZaduljitelnaLiteratura/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaduljitelnaLiteratura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double broistranici = double.Parse(Console.ReadLine());
+            double prochete = double.Parse(Console.ReadLine());
+            double broidni = double.Parse(Console.ReadLine());
+            double ivan = broistranici / prochete;
+            double drao = ivan / broidni;
+            Console.WriteLine(drao);
+            Console.ReadLine();
+        }
+    }
+}
