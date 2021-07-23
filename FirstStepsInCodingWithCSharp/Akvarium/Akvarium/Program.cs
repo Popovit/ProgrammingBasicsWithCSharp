c6fe474bab44bce8a72c360b91852e618dfa9f0f
Popovit <87820730+Popovit@users.noreply.github.com>
2021-07-23 15:19:53 +0300

    Add files via upload

---
+++ FirstStepsInCodingWithCSharp/Akvarium/Akvarium/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akvarium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double lenght = double.Parse(Console.ReadLine());   //Дължина в см.
+            double width = double.Parse(Console.ReadLine());// Широчина в см.
+            double height = double.Parse(Console.ReadLine());//Височина в см.
+            double procent = double.Parse(Console.ReadLine());//Процент.
+            double capacity = lenght * width * height;
+            double litersOfAquarium = capacity * 0.001;
+            double procentSum = procent * 0.01;
+            double litersSum = litersOfAquarium * (1 - procentSum);
+            Console.WriteLine(litersSum);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
