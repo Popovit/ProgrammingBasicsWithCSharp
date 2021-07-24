927bd31e34ded56c4be71db45504f131860dc867
Georgi Popov <87820730+Popovit@users.noreply.github.com>
2021-07-24 13:56:41 +0300

    Add files via upload

---
+++ FirstStepsInCodingWithCSharp/biggerNum/biggerNum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biggerNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            double num1 = double.Parse(Console.ReadLine());
+
+            if (num > num1)
+            {
+
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine(num1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
