241bb6dcbde52f6623229f8986b570089fee38ec
Georgi Popov <87820730+Popovit@users.noreply.github.com>
2021-07-24 14:25:35 +0300

    Add files via upload

---
+++ FirstStepsInCodingWithCSharp/poznaiParolata/poznaiParolata/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poznaiParolata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            if (password == "s3cr3t!P @ssw0rd")
+
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Pass");
+            }
+            Console.ReadLine();
+        
+        
+        }
+
+    }
+}
